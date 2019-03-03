9364b5dede5fc0eb83775e741de3b23bb7d5e07f
splendid-frame-7429 <bsameera@yahoo.com>
2019-03-03 07:16:55 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -10,7 +10,7 @@ end
 def get_japanese_emoticon(file_path, japanese_emoticon)
   # code goes here
   emoticons = YAML.load_file(file_path)
-  emoticons[]
+  emoticons['get_emoticon'][japanese_emoticon]
 end
 
 def get_english_meaning(file_path, english_emoticon)
