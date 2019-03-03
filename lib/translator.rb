e372538556796bbe4cedf38654e336acaedd9e1a
splendid-frame-7429 <bsameera@yahoo.com>
2019-03-03 06:38:32 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,18 @@
 # require modules here
+require "yaml"
 
-def load_library
+def load_library(file_path)
   # code goes here
+  emoticons = YAML.load_file(file_path)
+  emoticons
 end
 
-def get_japanese_emoticon
+def get_japanese_emoticon(file_path, japanese_emoticon)
   # code goes here
+  emoticons = YAML.load_file(file_path)
 end
 
-def get_english_meaning
+def get_english_meaning(file_path, english_emoticon)
   # code goes here
+  emoticons = YAML.load_file(file_path)
 end
