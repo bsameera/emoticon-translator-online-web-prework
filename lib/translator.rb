a84b9f3f7a10d4e07923ef1ccdc1e7c2bc72e433
splendid-frame-7429 <bsameera@yahoo.com>
2019-03-03 07:23:53 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,28 @@
 # require modules here
+require "yaml"
 
-def load_library
+def load_library(file_path)
   # code goes here
+  emoticons = YAML.load_file(file_path)
+  emoticons
 end
 
-def get_japanese_emoticon
+def get_japanese_emoticon(file_path, japanese_emoticon)
   # code goes here
+  emoticons = load_library(file_path)
+  if emoticons['get_emoticon'][japanese_emoticon] != nil 
+    return emoticons['get_emoticon'][japanese_emoticon]
+  else 
+    return "Sorry, that emoticon was not found"
+  end 
 end
 
-def get_english_meaning
+def get_english_meaning(file_path, english_emoticon)
   # code goes here
+  emoticons = load_library(file_path)
+  if emoticons['get_meaning'][english_emoticon] != nil 
+    return emoticons['get_meaning'][english_emoticon]
+  else 
+    return "Sorry, that emoticon was not found"
+  end 
 end
